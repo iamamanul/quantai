0b0b76dd1479685e33ec46c9640e4e4575c053cb
Amanul Hasan <amanulmat@gmail.com>
2025-08-25 11:32:48 +0530

    Update route.js

--- app/api/assessments/route.js
+++ app/api/assessments/route.js
@@ -1,6 +1,6 @@
 import { getAssessments } from "@/actions/interview";
 
-export async function GET() {
+export async function GET(req) {
   const data = await getAssessments();
   return Response.json(data);
-}
+} 
